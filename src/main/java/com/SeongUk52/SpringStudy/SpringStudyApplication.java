aadde9f14de9b7fad3bdcd683a2bb6dbf753151f
SeongUk52 <rkfka7849@gmail.com>
2024-01-10 19:44:47 +0900

    init

---
+++ src/main/java/com/SeongUk52/SpringStudy/SpringStudyApplication.java
@@ -0,0 +1,13 @@
+package com.SeongUk52.SpringStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SeongUk52/SpringStudy/SpringStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SeongUk52.SpringStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
