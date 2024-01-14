ce497cb16127c0e63f6d48999604ae281a02e4ab
SeongUk52 <rkfka7849@gmail.com>
2024-01-14 22:14:39 +0900

    feat(Member): 회원관리 도메인, 레퍼지토리

---
+++ src/main/java/com/SeongUk52/SpringStudy/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.SeongUk52.SpringStudy.repository;
+
+import com.SeongUk52.SpringStudy.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findByID(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
