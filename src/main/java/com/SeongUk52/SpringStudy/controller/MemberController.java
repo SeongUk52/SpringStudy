64e1d5321329141039df1e4867657612d90f78e9
SeongUk52 <rkfka7849@gmail.com>
2024-01-23 21:09:03 +0900

    feat(MemberController): 회원 조회 기능

--- src/main/java/com/SeongUk52/SpringStudy/controller/MemberController.java
+++ src/main/java/com/SeongUk52/SpringStudy/controller/MemberController.java
@@ -2,8 +2,10 @@ package com.SeongUk52.SpringStudy.controller;
 
 import com.SeongUk52.SpringStudy.domain.Member;
 import com.SeongUk52.SpringStudy.service.MemberService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -30,5 +32,12 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping(value = "/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
